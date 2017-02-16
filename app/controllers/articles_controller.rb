5c8ccfe06ea9ce3dd8be8946a704f4cd49024c0c
DevByWater <egaraudy@gmail.com>
2017-02-16 12:40:53 -0700

    updating articles destroy method and moved forms and messages to partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,42 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
+  end
+
   def new
+    @article = Article.new
+
+
+  end
+
+  def edit
+
+    @article = Article.find(params[:id])
 
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to @article
+    else
+      render 'new'
+    end
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to @article
+    end
   end
 
   private
